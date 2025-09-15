c2bed2ff5377ccc962a7c49375b070d521b3572c
zainkhan456 <zainulislamk456@gmail.com>
2025-09-15 14:42:32 +0500

    Arrow functions in dart

---
+++ dart-basics/08_Functions/05_Arrow_Function/program01.dart
@@ -0,0 +1,7 @@
+//Also called Lambda function
+//Arrow Fuction => Used when a function has one line of code
+//sum of two numbers 
+void sum(a,b) => print(a + b);
+void main(){
+sum(4, 2);
+}
---
+++ dart-basics/08_Functions/05_Arrow_Function/program02.dart
@@ -0,0 +1,7 @@
+//sum of two number
+sum(a,b) => a + b;
+void main(){
+  // print(sum(8,9));// or 
+  int result = sum(8, 9);
+  print(result);
+}
